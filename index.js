144d56d629354fbf8f01481687bb3fad624f4c5f
zicla <ziclax@126.com>
2018-04-09 15:35:59 +0800

    Fix some log details.

--- index.js
+++ index.js
@@ -2,34 +2,28 @@ var chokidar = require('chokidar');
 const {exec} = require('child_process');
 // One-liner for current directory, ignores .dotfiles
 chokidar.watch('.', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
-    console.log(event, path);
-
 
     if (path.endsWith(".less") && !path.startsWith("node_modules/")) {
 
-        console.log("run...");
-
-        console.log(process.execPath)
-        console.log(__dirname)
-        console.log(process.cwd())
         let lesscPath = __dirname + "/node_modules/.bin/lessc";
         let srcPath = __dirname + "/" + path;
         let distPath = __dirname + "/" + path.replace(new RegExp('less$'), 'css');
-
-        exec(lesscPath + ' ' + srcPath + ' ' + distPath, (err, stdout, stderr) => {
-            console.log("err");
-            console.log(err);
-            console.log(stdout);
-            console.log(stderr);
-
+        let command = lesscPath + ' ' + srcPath + ' ' + distPath;
+        console.log(command);
+        exec(command, (err, stdout, stderr) => {
             if (err) {
-                // node couldn't execute the command
-                return;
+
+                console.log(err);
+            }
+            if (stdout) {
+
+                console.log(stdout);
+            }
+            if (stderr) {
+
+                console.log(stderr);
             }
 
-            // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
         });
     }
 
